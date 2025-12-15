6217d9d79086a06462b1215eae85336b5177bce8
daris <daris91@gmail.com>
2025-12-15 13:38:40 +0100

    code cleanup

--- src/main/java/com/example/airquality/airquality/AirQualityPjpStationMapper.java
+++ src/main/java/com/example/airquality/airquality/AirQualityPjpStationMapper.java
@@ -1,5 +1,6 @@
 package com.example.airquality.airquality;
 
+import com.example.airquality.pjpapi.dto.AqIndexDto;
 import com.example.airquality.pjpapi.dto.AqIndexResponse;
 import com.example.airquality.pjpapi.dto.PjpStationDto;
 import lombok.RequiredArgsConstructor;
@@ -13,12 +14,14 @@ public class AirQualityPjpStationMapper {
     public AirQualityStation toEntity(PjpStationDto pjpStationDto, AqIndexResponse aqIndexResponse) {
         AirQualityStation airQualityStation = new AirQualityStation();
         airQualityStation.setCity(pjpStationDto.cityName());
-        airQualityStation.setOverallIndex(airQualityIndexMapper.map(aqIndexResponse.aqIndex().indexCategory()));
-        airQualityStation.setSo2Index(airQualityIndexMapper.map(aqIndexResponse.aqIndex().so2Category()));
-        airQualityStation.setNo2Index(airQualityIndexMapper.map(aqIndexResponse.aqIndex().no2Category()));
-        airQualityStation.setPm10Index(airQualityIndexMapper.map(aqIndexResponse.aqIndex().pm10Category()));
-        airQualityStation.setPm25Index(airQualityIndexMapper.map(aqIndexResponse.aqIndex().pm25Category()));
-        airQualityStation.setO3Index(airQualityIndexMapper.map(aqIndexResponse.aqIndex().o3Category()));
+
+        AqIndexDto aqIndexDto = aqIndexResponse.aqIndex();
+        airQualityStation.setOverallIndex(airQualityIndexMapper.map(aqIndexDto.indexCategory()));
+        airQualityStation.setSo2Index(airQualityIndexMapper.map(aqIndexDto.so2Category()));
+        airQualityStation.setNo2Index(airQualityIndexMapper.map(aqIndexDto.no2Category()));
+        airQualityStation.setPm10Index(airQualityIndexMapper.map(aqIndexDto.pm10Category()));
+        airQualityStation.setPm25Index(airQualityIndexMapper.map(aqIndexDto.pm25Category()));
+        airQualityStation.setO3Index(airQualityIndexMapper.map(aqIndexDto.o3Category()));
 
         return airQualityStation;
     }
